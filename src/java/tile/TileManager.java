058b8d5b416d70b2d575608f3fc6d3de1f8b370c
= <amc1140@aol.com>
2022-08-27 08:47:24 -0700

    Adding a Hut tile

--- src/java/tile/TileManager.java
+++ src/java/tile/TileManager.java
@@ -42,11 +42,21 @@ public class TileManager {
             Tile road = new Tile(roadImage, true);
             tiles[1] = road;
 
+            // Sidewalk
+            BufferedImage sidewalkImage = ImageIO.read(new File("/Users/aaroncorona/eclipse-workspace/GTA/src/assets/images/tiles/sidewalk.png"));
+            Tile sidewalk = new Tile(sidewalkImage, true);
+            tiles[2] = sidewalk;
+
             // Tree
             BufferedImage treeImage = ImageIO.read(new File("/Users/aaroncorona/eclipse-workspace/GTA/src/assets/images/tiles/tree.png"));
             Tile tree = new Tile(treeImage, true);
-            tiles[2] = tree;
-            // @TODO create building sprite
+            tiles[3] = tree;
+
+            // Hut
+            BufferedImage hutImage = ImageIO.read(new File("/Users/aaroncorona/eclipse-workspace/GTA/src/assets/images/tiles/hut.png"));
+            Tile hut = new Tile(hutImage, true);
+            tiles[4] = hut;
+
 
         } catch (IOException e) {}
     }
@@ -57,9 +67,10 @@ public class TileManager {
 
         // @TODO create mapping text file
         g.drawImage(tiles[0].image, 150, 150, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
-        g.drawImage(tiles[1].image, 200, 200, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
-        g.drawImage(tiles[2].image, 250, 250, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
-
+        g.drawImage(tiles[1].image, 200, 150, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
+        g.drawImage(tiles[2].image, 250, 150, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
+        g.drawImage(tiles[3].image, 300, 150, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
+        g.drawImage(tiles[5].image, 350, 150, panel.UNIT_SIZE, panel.UNIT_SIZE, null);
 
     }
 }
