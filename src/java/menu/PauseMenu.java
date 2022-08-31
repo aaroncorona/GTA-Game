b50336b778944d6eaaa916c3cb002a83a415a7b7
= <amc1140@aol.com>
2022-08-31 14:19:16 -0700

    Updating the pause menu to add Buttons

--- src/java/menu/PauseMenu.java
+++ src/java/menu/PauseMenu.java
@@ -1,25 +1,42 @@
 package menu;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static java.awt.Component.CENTER_ALIGNMENT;
 
 public class PauseMenu implements Menu {
 
     public final JPopupMenu pauseMenu;
     int xPos, yPos;
     int width, height;
-    JMenuItem item1;
+    JLabel label;
+    JButton button1, button2, button3, button4;
+    BufferedImage image;
 
     // Constructor
     public PauseMenu() {
 
-        // Instantiate menu objects
+        // Instantiate menu and related objects
         pauseMenu = new JPopupMenu();
-        JMenuItem item1 = new JMenuItem("Option 1");
-        pauseMenu.add(item1);
+
+        // Menu label
+        label = new JLabel("PAUSED");
+        pauseMenu.add(label);
+
+        // Menu Items
+        button1 = new JButton("Resume");
+        pauseMenu.add(button1);
+        button2 = new JButton("Controls");
+        pauseMenu.add(button2);
+        button3 = new JButton("Restart");
+        pauseMenu.add(button3);
+        button4 = new JButton("Quit");
+        pauseMenu.add(button4);
 
         setDefaultValues();
     }
@@ -27,18 +44,29 @@ public class PauseMenu implements Menu {
     @Override
     public void setDefaultValues() {
         // Placement vars
-        xPos = 500;
-        yPos = 500;
+        xPos = 650;
+        yPos = 300;
         width = 400;
         height = 100;
 
-        // Default Menu UI settings
+        // Menu UI
         pauseMenu.setLocation(xPos, yPos);
-        pauseMenu.setPreferredSize(new Dimension(500, 200));
-        pauseMenu.setBackground(Color.WHITE);
-        pauseMenu.setBorder(BorderFactory.createLineBorder(Color.PINK, 3));
+        pauseMenu.setPreferredSize(new Dimension(400, 170));
+        pauseMenu.setBackground(Color.CYAN);
+        pauseMenu.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
         pauseMenu.setFocusable(false); // Prevent the menu from taking focus from the panel
         closeMenu();
+
+        // Label UI
+        label.setFont(new Font("Verdana", Font.PLAIN, 30));
+        label.setAlignmentX(Panel.CENTER_ALIGNMENT);
+        label.setAlignmentY(Panel.CENTER_ALIGNMENT);
+
+        // Button UI
+        button1.setAlignmentX(Panel.CENTER_ALIGNMENT);
+        button2.setAlignmentX(Panel.CENTER_ALIGNMENT);
+        button3.setAlignmentX(Panel.CENTER_ALIGNMENT);
+        button4.setAlignmentX(Panel.CENTER_ALIGNMENT);
     }
 
     @Override
@@ -61,9 +89,6 @@ public class PauseMenu implements Menu {
 
     }
 }
-
-// Add JMenuItems
-
 
 //        pauseMenu.add(new JMenuItem(new AbstractAction("Option 2") {
 //            public void actionPerformed(ActionEvent e) {
